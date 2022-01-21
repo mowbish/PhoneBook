50e0cb65aa94338cffed5edbdc460e176fa7d37a
Mobin <mobin.clash.ma@gmail.com>
2022-01-21 15:22:35 +0330

    Added new path for show all contacts

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 
-from contacts.views import ContactAPIView, CreateContactAPIView
+from contacts.views import ContactAPIView, CreateContactAPIView, ShowAllContactsAPIView
 
 urlpatterns = [
-    path('<int:pk>', ContactAPIView.as_view(), name='contacts'),
+    path('', ShowAllContactsAPIView.as_view(), name='contacts'),
+    path('<int:phone_number>', ContactAPIView.as_view(), name='contact'),
     path('create/', CreateContactAPIView.as_view(), name='create'),
 ]
