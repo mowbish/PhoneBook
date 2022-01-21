65ee96cad5af8a8c295fdef59ccbd2c8f1781029
Mobin <mobin.clash.ma@gmail.com>
2022-01-21 14:58:02 +0330

    Added view for Create and update contact

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from contacts.models import Contact, Phone
+from contacts.serializers import ContactSerializer, CreateContactSerializer
 
-# Create your views here.
+
+class CreateContactAPIView(ListCreateAPIView):
+    serializer_class = CreateContactSerializer
+    queryset = Contact.objects.all()
+
+
+class ContactAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all()
