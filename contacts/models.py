3c3850c9217397e67290523f42fb5db702382ab9
Mobin <mobin.clash.ma@gmail.com>
2022-01-21 11:15:44 +0330

    Added content of contents app modeling

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class Contact(User):
+
+    def __str__(self):
+        return self.username
+
+
+class Phone(models.Model):
+    contact = models.ForeignKey(Contact)
+    phone_number_regex = RegexValidator(regex=r"^\+?1?\d{8,15}$", message='Must enter a valid phone number')
+    phone_number = models.CharField(validators=[phone_number_regex], max_length=16, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
