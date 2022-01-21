29ad078dcf4fcd4c818ff6d5a491c5c353ae7646
Mobin <mobin.clash.ma@gmail.com>
2022-01-21 21:03:14 +0330

    Path for SignUp and create contact and contact list

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from contacts.views import ContactAPIView, CreateContactAPIView, ShowAllContactsAPIView
+from contacts.views import ClientAPIView, SignUpAPIView, CreateContactAPIView,CreateContactListAPIView
 
 urlpatterns = [
-    path('', ShowAllContactsAPIView.as_view(), name='contacts'),
-    path('<int:phone_number>', ContactAPIView.as_view(), name='contact'),
-    path('create/', CreateContactAPIView.as_view(), name='create'),
+
+    path('signup/', SignUpAPIView.as_view(), name='signup'),
+    path('create/contact', CreateContactAPIView.as_view(), name='create-contact-list'),
+    path('create/contact-list', CreateContactListAPIView.as_view(), name='create-contact-list'),
+    path('<str:name>', ClientAPIView.as_view(), name='contact'),
 ]
