768231a301c656c299f33cadf418f3bb82d49736
Mobin <mobin.clash.ma@gmail.com>
2022-01-22 15:12:54 +0330

    Change DATABASE to POSTGRESQL

--- PhoneBook/settings.py
+++ PhoneBook/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'PhoneBook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'PhoneBook',
+        'USER': 'postgres',
+        'PASSWORD': 'mobinmobin1380',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
