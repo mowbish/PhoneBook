ef5d4a2e346f138c990e202b9d6d810e26878575
mobin <mobin.clash.ma@gmail.com>
2022-01-25 10:30:29 +0330

    Changes about testing

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,19 +1,53 @@
 from django.test import TestCase
-
-from contacts.models import User
+from django.urls import reverse
+from .models import User
+import json
+from django.test import Client
+from rest_framework import status
+client = Client()
 
 
 class SignUpTestCase(TestCase):
     def setUp(self):
-        User.objects.create(name="lion", sound="roar")
-        User.objects.create(name="cat", sound="meow")
-
-    def test_can_register(self):
-        """Animals that can speak are correctly identified"""
-        data = {
-            'Phone_number' : "s"
+        self.valid_payload = {
+            "phone_number": "9999255351",
+            "first_name": "Mobin",
+            "last_name": "Aghaei",
+            "email": "mobin.agaei@gmail.com",
         }
-        self.client.post('signup', data=data)
-        # cat = Animal.objects.get(name="cat")
-        # self.assertEqual(lion.speak(), 'The lion says "roar"')
-        # self.assertEqual(cat.speak(), 'The cat says "meow"')
+        self.invalid_payload = {
+            "phone_number": "mzdf",
+            "first_name": "",
+            "last_name": "",
+            "email": "",
+        }
+    def test_valid_sign_up(self):
+        response = client.post(
+            reverse('signup'),
+            data = json.dumps(self.valid_payload),
+            content_type='application/json',
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+    def test_invalid_sign_up(self):
+        response = client.post(
+            reverse('signup'),
+            data = json.dumps(self.invalid_payload),
+            content_type='application/json',
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+class GetContactDetailTest(TestCase):
+    def setUp(self):
+        self.mobin = User.objects.create(
+                    phone_number='9999255351',
+                    first_name='Mobin',
+                    last_name='Aghaei',
+                    email='mobin.agaei@gmail.com',
+                )
+
+    def test_get_valid_contact_detail(self):
+        pass
